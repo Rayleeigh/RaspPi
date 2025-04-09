03f1dda9598651287a153b3baff3671314da1e73
Arlind-dev <arlind@sulej.ch>
2025-04-09 15:44:22 +0200

    test

--- setup.sh
+++ setup.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-mkdir -p ~/lab
+sudo mkdir -p ~/lab
 sudo mkdir -p /mnt/schnuppi
 
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install git
 
-curl -sSL https://get.docker.com | sh
+sudo curl -sSL https://get.docker.com | sh
 
 sudo usermod -aG docker $USER
 
-docker network create --subnet=192.168.100.0/24 private_lab
+sudo docker network create --subnet=192.168.100.0/24 private_lab
 
-git clone https://github.com/Rayleeigh/RaspPi ~/RaspPi
+sudo git clone https://github.com/Rayleeigh/RaspPi ~/RaspPi
 
-mv ~/RaspPi/installation_files/* ~/lab
+sudo mv ~/RaspPi/installation_files/* ~/lab
 
-rm -rf ~/RaspPi
+sudo rm -rf ~/RaspPi
 
-sed -i "s|<SECRET_ENCRYPTION_KEY>|$(openssl rand -hex 32)|" ~/lab/homarr/docker-compose.yml
+sudo sed -i "s|<SECRET_ENCRYPTION_KEY>|$(openssl rand -hex 32)|" ~/lab/homarr/docker-compose.yml
 
-rm ~/lab/dnsmasq/config/dnsmasq.conf.initial ~/lab/dnsmasq/config/dnsmasq.conf.template
+sudo rm ~/lab/dnsmasq/config/dnsmasq.conf.initial ~/lab/dnsmasq/config/dnsmasq.conf.template
 
-mv ~/lab/dnsmasq/config/dnsmasq.conf.final ~/lab/dnsmasq/config/dnsmasq.conf
+sudo mv ~/lab/dnsmasq/config/dnsmasq.conf.final ~/lab/dnsmasq/config/dnsmasq.conf
+
+sudo docker pull ghcr.io/ajnart/homarr:0.15.10
+sudo docker pull jasjeev4/rpi-dnsmasq:latest
+sudo docker pull nginx:1.27.4
+sudo docker pull ghcr.io/servercontainers/samba:latest
+sudo docker pull openspeedtest/latest:v2.0.6
+
+sudo echo "nameserver 192.168.100.10" | sudo tee /etc/resolvconf/resolv.conf.d/base
+sudo resolvconf -u
+
+sudo chmod -R 755 /mnt/schnuppi
+sudo chown -R 1000:1000 /mnt/schnuppi
+
+sudo find . -type f -exec sudo chmod 655 {} +
+sudo find . -type d -exec sudo chmod 755 {} +
+
+(cd ~/lab/homarr && sudo docker compose up -d)
+(cd ~/lab/samba && sudo docker compose up -d)
+(cd ~/lab/nginx && sudo docker compose up -d)
+(cd ~/lab/speedtest && sudo docker compose up -d)
+(cd ~/lab/dnsmasq && sudo docker compose up -d)
+
+sudo reboot
